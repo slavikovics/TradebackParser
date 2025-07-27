0ef1be2269977643aaed8a37fa3206092ab7e805
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-07-28 00:07:56 +0300

    Example of starting animation from code behind

--- TradebackParser/Views/MainView.axaml.cs
+++ TradebackParser/Views/MainView.axaml.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -8,5 +10,12 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        StartAnimation();
+    }
+
+    public async Task StartAnimation()
+    {
+        var animation = (Animation)this.Resources["PhaseCompletedAnimation"];
+        await animation.RunAsync(WebsiteBorder);
     }
 }
