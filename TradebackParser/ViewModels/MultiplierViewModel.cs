a38d451951a5f3a1c2a96155489fe841d4f22846
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-08-04 03:31:22 +0300

    Adding preview of excluded phrases

--- TradebackParser/ViewModels/MultiplierViewModel.cs
+++ TradebackParser/ViewModels/MultiplierViewModel.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Avalonia.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace TradebackParser.ViewModels;
@@ -13,11 +16,94 @@ public partial class MultiplierViewModel : ViewModelBase
     
     [ObservableProperty]
     private bool _isPhraseFilterActive = true;
+
+    [ObservableProperty]
+    private double _firstCountValue;
+    
+    [ObservableProperty]
+    private double _secondCountValue;
+    
+    [ObservableProperty]
+    //[NotifyPropertyChangedFor(nameof(Phrases))]
+    private string _phrasesFilter = string.Empty;
+    
+    public ObservableCollection<string> Phrases { get; set; }
     
     public List<ItemModel> Items { get; set; }
 
     public MultiplierViewModel(List<ItemModel> items)
     {
         Items = items;
+        Phrases = new ObservableCollection<string>();
+    }
+
+    partial void OnPhrasesFilterChanged(string phrase)
+    {
+        Phrases.Clear();
+        FindPhrases().ForEach(x => Phrases.Add(x));
+    }
+
+    private void RemoveFirstCount()
+    {
+        if (!IsFirstCountRuleActive) return;
+        List<ItemModel> itemsToRemove = new List<ItemModel>();
+        
+        foreach (var item in Items)
+        {
+            if (item.Count1 < FirstCountValue)
+            {
+                itemsToRemove.Add(item);
+            }
+        }
+        
+        itemsToRemove.ForEach(item => Items.Remove(item));
+    }
+
+    private void RemoveSecondCount()
+    {
+        if (!IsSecondCountRuleActive) return;
+        List<ItemModel> itemsToRemove = new List<ItemModel>();
+        
+        foreach (var item in Items)
+        {
+            if (item.Count2 < SecondCountValue)
+            {
+                itemsToRemove.Add(item);
+            }
+        }
+        
+        itemsToRemove.ForEach(item => Items.Remove(item));
+    }
+
+    private List<string> FindPhrases()
+    {
+        List<string> phrases = PhrasesFilter.Split(' ').ToList();
+
+        for (int i = 0; i < phrases.Count; i++)
+        {
+            phrases[i] = phrases[i].Trim(' ');
+            phrases[i] = phrases[i].Replace("\"", "");
+        }
+
+        return phrases;
+    }
+
+    private void RemovePhraseFilter()
+    {
+        var phrases = FindPhrases();
+        List<ItemModel> itemsToRemove = new List<ItemModel>();
+        
+        foreach (var item in Items)
+        {
+            foreach (var phrase in phrases)
+            {
+                if (item.Name.Contains(phrase))
+                {
+                    itemsToRemove.Add(item);
+                }
+            }
+        }
+        
+        itemsToRemove.ForEach(item => Items.Remove(item));
     }
 }
